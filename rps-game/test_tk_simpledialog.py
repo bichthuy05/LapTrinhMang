944c3e3f22c09515b5819ae99ffbdffa8107cc26
bichthuy05 <163622227+bichthuy05@users.noreply.github.com>
2025-08-28 15:29:13 +0700

    Delete rps-game directory

--- rps-game/test_tk_simpledialog.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import simpledialog, messagebox
-
-root = tk.Tk()
-root.withdraw()
-name = simpledialog.askstring("Test", "Nhập tên của bạn:")
-print("Bạn đã nhập:", name)
-root.destroy()
